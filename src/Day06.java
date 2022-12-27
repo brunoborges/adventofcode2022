e257aa6a56b256b8a8a0c3144264cf46bcf5f9b0
Bruno Borges <bruno.borges@gmail.com>
2022-12-26 21:41:53 -0300

    day 6 part 2

--- src/Day06.java
+++ src/Day06.java
@@ -5,12 +5,21 @@ public class Day06 {
 	public static void main(String[] args) {
 		var input = readInputFromFile();
 		char[] data = input.toCharArray();
+
+		var sop_marker = findMarker(input, data, 4);
+		var msg_marker = findMarker(input, data, 14);
+
+		System.out.println("found start-of-packet marker at: " + sop_marker);
+		System.out.println("found message marker at: " + msg_marker);
+	}
+
+	static int findMarker(String body, char[] data, int grouping) {
 		var bufferCounter = 0;
-		char[] buffer = new char[4];
+		char[] buffer = new char[grouping];
 		for (int i = 0; i < data.length; i++) {
 			buffer[bufferCounter] = data[i];
 			bufferCounter++;
-			if (bufferCounter == 4) {
+			if (bufferCounter == grouping) {
 				bufferCounter = 0;
 				i = i - 3; // reset to the next char in original input data
 				boolean foundDuplicate = false;
@@ -29,12 +38,13 @@ public class Day06 {
 
 				if (foundDuplicate == false) {
 					var toString = new String(buffer);
-					var indexOf = input.indexOf(toString) + toString.length();
-					System.out.println("found unique: " + toString + " at marker " + indexOf);
-					break;
+					var indexOf = body.indexOf(toString) + toString.length();
+					return indexOf;
 				}
 			}
 		}
+
+		return -1;
 	}
 
 	static String readInputFromFile() {
